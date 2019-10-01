9e09814af4a9c145f0bcc368cbda6361b80b1335
Roberto Solares <betosolaresgar@gmail.com>
2019-09-30 19:18:22 -0600

    Prepare the solution

---
+++ encryption/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Optimization;
+
+namespace encryption
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // CSS
+            //bundles.Add(new StyleBundle("~/bundles/style").Include());
+
+            // JS
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                       "~/Scripts/jquery-3.4.1.min.js"));
+        }
+    }
+}
