6d31dac726f4c993056087645b0f50c43d98626b
Roberto Solares <betosolaresgar@gmail.com>
2019-10-01 20:22:36 -0600

    Add the structure for the encription

--- encryption/Controllers/EncryptController.cs
+++ encryption/Controllers/EncryptController.cs
@@ -1,15 +1,82 @@
-﻿using System.Web.Mvc;
+﻿using encryption.Utils;
+using System.Web;
+using System.Web.Mvc;
 
 namespace encryption.Controllers
 {
     public class EncryptController : Controller
     {
+        private readonly FileUtils fileUtils = new FileUtils();
+
         // Default view for the encription
         // GET: /Encrypt , /Encrypt/Index
+        [HttpGet]
         public ActionResult Index()
         {
             ViewBag.Message = string.Empty;
             return View();
+        }
+
+        // Encrypt the file uploaded
+        [HttpPost]
+        public ActionResult Encrypt(HttpPostedFileBase file, string cipher, string key)
+        {
+            string error = string.Empty;
+            string path = string.Empty;
+            if (DidEncription(file, ref error, ref path, cipher, key))
+            {
+                ViewBag.Message = "SUCCESS";
+                return fileUtils.DownloadFile(path);
+            }
+            else
+            {
+                ViewBag.Message = error;
+                return View("Index");
+            }
+        }
+
+        /// <summary>Try to encrypt the file with the specific cipher algorithm</summary>
+        /// <param name="file">The file uploaded</param>
+        /// <param name="error">The error to send back</param>
+        /// <param name="path">The path to the encrypted file</param>
+        /// <param name="cipher">The cipher algorithm to use</param>
+        /// <param name="key">The key for the encription</param>
+        /// <returns>True if the file was encripted, otherwise false</returns>
+        private bool DidEncription(HttpPostedFileBase file, ref string error, ref string path, string cipher, string key)
+        {
+            if (fileUtils.IsFileTypeCorrect(file, ".txt", ref error))
+            {
+                string uploadedPath = fileUtils.SaveFile(file, "~/App_Data/Uploads");
+                if (fileUtils.IsFileEmpty(uploadedPath))
+                {
+                    error = "Empty file";
+                    return false;
+                }
+                else
+                {
+                    if (cipher.Equals("Caesar"))
+                    {
+                        // Caesar encription
+                    }
+                    else
+                    {
+                        int numericKey = int.Parse(key);
+                        if (cipher.Equals("ZigZag"))
+                        {
+                            // ZigZag encription
+                        }
+                        else
+                        {
+                            // Spiral encription
+                        }
+                    }
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
