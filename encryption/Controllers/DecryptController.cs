3b82b7f98fe3a2f7270193aa2ed430bf5849e035
Roberto Solares <betosolaresgar@gmail.com>
2019-10-01 19:51:18 -0600

    Make the decryption view

---
+++ encryption/Controllers/DecryptController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace encryption.Controllers
+{
+    public class DecryptController : Controller
+    {
+        // Default view for the decription
+        // GET: /Decrypt , /Decrypt/Index
+        public ActionResult Index()
+        {
+            ViewBag.Message = string.Empty;
+            return View();
+        }
+    }
+}
