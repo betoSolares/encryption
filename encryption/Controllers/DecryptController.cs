18b3066a25ee39771f2969bde54f7be2c1969a30
Roberto Solares <betosolaresgar@gmail.com>
2019-10-02 18:50:54 -0600

    Make the decryption for the zigzag

--- encryption/Controllers/DecryptController.cs
+++ encryption/Controllers/DecryptController.cs
@@ -63,7 +63,17 @@ namespace encryption.Controllers
                         int numericKey = int.Parse(key);
                         if (cipher.Equals("ZigZag"))
                         {
-                            // ZigZag decription
+                            ZigZagUtils zigZag = new ZigZagUtils();
+                            try
+                            {
+                                zigZag.Decrypt(uploadedPath, numericKey, ref path);
+                                return true;
+                            }
+                            catch
+                            {
+                                error = "Bad Encryption";
+                                return false;
+                            }
                         }
                         else
                         {
--- encryption/Utils/ZigZagUtils.cs
+++ encryption/Utils/ZigZagUtils.cs
@@ -8,12 +8,50 @@ namespace encryption.Utils
         /// <summary>Encrypt the message in the file</summary>
         /// <param name="path">The path to the file</param>
         /// <param name="key">The key for the encription</param>
+        /// <param name="newPath">The path of the new file</param>
         public void Encrypt(string path, int key, ref string newPath)
         {
             int length = GetLength(path);
             char[,] matrix = CreateMatrix(key, length);
             matrix = FillMatrix(matrix, key, path);
             newPath = WriteEncryptedMessage(matrix, key, length);
+        }
+
+        /// <summary>Decrypt the message in the file</summary>
+        /// <param name="path">The path to the file</param>
+        /// <param name="key">The key for the encription</param>
+        /// <param name="newPath">The path of the new file</param>
+        public void Decrypt(string path, int key, ref string newPath)
+        {
+            int length = GetLength(path);
+            char[,] matrix = CreateMatrix(key, length);
+            matrix = MarkPlaces(matrix, length, key);
+            matrix = ConstructMatrix(matrix, key, length, path);
+            newPath = WriteDecryptedMessage(matrix, key, length);
+        }
+
+        /// <summary>Fill the matrix with the characters in the file</summary>
+        /// <param name="matrix">The matrix to fill</param>
+        /// <param name="key">The key for the encryption</param>
+        /// <param name="length">The length of the text</param>
+        /// <param name="path">The path to the file to read</param>
+        /// <returns>A matrix filled with the characters in the file</returns>
+        private char[,] ConstructMatrix(char[,] matrix, int key, int length, string path)
+        {
+            int index = 0;
+            string text = File.ReadAllText(path);
+            for (int i = 0; i < key; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    if (matrix[i, j].Equals(Convert.ToChar(2)) && index < length)
+                    {
+                        matrix[i, j] = text[index];
+                        index++;
+                    }
+                }
+            }
+            return matrix;
         }
 
         /// <summary>Create the matrix</summary>
@@ -69,6 +107,64 @@ namespace encryption.Utils
             int length = reader.ReadToEnd().Length;
             reader.Close();
             return length;
+        }
+
+        /// <summary>Mark the places to put the characters</summary>
+        /// <param name="matrix">The matrix to fill</param>
+        /// <param name="length">The length of the text</param>
+        /// <param name="key">The key for the decompretion</param>
+        /// <returns>A matrix filled with the new characters</returns>
+        private char[,] MarkPlaces(char[,] matrix, int length, int key)
+        {
+            bool isDirectionDown = false;
+            int row = 0;
+            int columns = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if (row == 0)
+                    isDirectionDown = true;
+                if (row == key - 1)
+                    isDirectionDown = false;
+                matrix[row, columns] = Convert.ToChar(2);
+                if (isDirectionDown)
+                    row++;
+                else
+                    row--;
+                columns++;
+            }
+            return matrix;
+        }
+
+        /// <summary>Iterate through the matrix and write the message</summary>
+        /// <param name="matrix">The matrix to iterate</param>
+        /// <param name="key">The key for the decription</param>
+        /// <param name="length">The length of the text</param>
+        /// <returns>The path of the new file</returns>
+        private string WriteDecryptedMessage(char[,] matrix, int key, int length)
+        {
+            string path = new FileUtils().CreateFile("DecryptedMessage", ".txt", "~/App_Data/Downloads");
+            bool isDirectionDown = false;
+            int row = 0;
+            int columns = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if (row == 0)
+                    isDirectionDown = true;
+                if (row == key - 1)
+                    isDirectionDown = false;
+                if (!matrix[row, columns].Equals(Convert.ToChar(2)))
+                {
+                    StreamWriter writer = new StreamWriter(path, true);
+                    writer.Write(matrix[row, columns]);
+                    writer.Close();
+                }
+                if (isDirectionDown)
+                    row++;
+                else
+                    row--;
+                columns++;
+            }
+            return path;
         }
 
         /// <summary>Iterate through the matrix and write the message</summary>
