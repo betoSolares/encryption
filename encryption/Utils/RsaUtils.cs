468b9e93e4e8f3a39997d93fa5de5ef145dd0bb3
Brenner Hernández <velasquezmochis@gmail.com>
2019-10-31 10:58:19 -0600

    Verifies that the numbers are prime

--- encryption/Utils/RsaUtils.cs
+++ encryption/Utils/RsaUtils.cs
@@ -2,5 +2,20 @@
 {
     public class RsaUtils
     {
+        /// <summary>Check if a number is prime or not</summary>
+        /// <param name="number">The number to check</param>
+        /// <returns>True if it's prime, otherwise false</returns>
+        private bool IsPrime(int number)
+        {
+            int cont = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                    cont++;
+            }
+            if (cont == 2)
+                return true;
+            return false;
+        }
     }
 }
