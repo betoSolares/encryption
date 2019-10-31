27c6bb9e55d15a807039da9c2c817274f32c95a8
Brenner Hernández <velasquezmochis@gmail.com>
2019-10-31 11:07:41 -0600

    Calculates greatest common divisor between e and phi

--- encryption/Utils/RsaUtils.cs
+++ encryption/Utils/RsaUtils.cs
@@ -17,5 +17,30 @@
                 return true;
             return false;
         }
+
+        /// <summary>Calculate the greatest common divisor of two integers</summary>
+        /// <param name="a">The first integer</param>
+        /// <param name="b">The second integer</param>
+        /// <returns>The greatest common divisor</returns>
+        private ulong GCD(ulong a, ulong b)
+        {
+            bool isFound = false;
+            ulong temporal;
+            while (!isFound)
+            {
+                temporal = a % b;
+                if (temporal == 0)
+                {
+                    isFound = true;
+                }
+                else
+                {
+                    a = b;
+                    b = temporal;
+                }
+            }
+            return b;
+        }        
+
     }
 }
